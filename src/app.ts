e56bc2d86869b93bca3617a5745ee0c776ba180d
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-03 11:36:58 +0530

    app.js update

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
+
+
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
 
 const app: Express = express();
 
+app.use(helmet());
+app.use(express.json({limit:'2mb'}));
+app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+app.use(cookieParser());
+
+app.use(mongoSanitize());
 
 
 export default app;
